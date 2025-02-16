39b9dfb0deb9e8255bf9f68ceedb384311c7ded9
Ethan R <ethanrudkin@rhs.school.nz>
2025-02-17 10:57:48 +1300

    02_input_math.py

--- 02_input_math.py
+++ 02_input_math.py
@@ -29,11 +29,15 @@ num_1 = int(input("What is your favourite number? "))
 num_2 = int(input("What is your second favourite number?"))
 
 # add numbers together
-add = num_1 + num_2
+add = num_1 + num_2 
 
 # multiply numbers together
+multi = num_1 * num_2
 
 # greet user and display math
 print("Hello", name)
 
 print("{} + {} = {}".format(num_1, num_2, add))
+print("{} * {} = {}".format(num_1, num_2, multi))
+
+print(f'{num_1} * {num_2} = {multi}')
